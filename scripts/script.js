d720175e288f7f0c8ec4e6991ca45202dff540da
xinranw <xinranw@seas.upenn.edu>
2013-07-17 02:07:43 -0400

    v2.3
    Code cleanup, new icons, added javascript for navbar highlighting

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,35 @@
 		.slideUp(200);
 	})
 })();
+
+function auto_run(){
+  add_slider_effect;
+  load_nav_button();
+  highlight_nav_links();
+}
+
+function load_nav_button(){
+  $(".nav-button").click(function () {
+    $(".nav-button,.nav").toggleClass("open");
+  });    
+}
+
+function highlight_nav_links(){
+  function find_a(text){
+    return $(this).text() === text;
+  }
+
+  var url = $(location).attr('href');
+  var start = url.lastIndexOf('/') + 1;
+  var linkName = url.substring(start);
+  var link = $('a[href$="' + linkName + '"]');
+  link.addClass("selected");
+  link.parents(".dropdown").children('a[href$="#"]').addClass("selected")
+}
+
+function add_slider_effect(){
+  $('#slider').nivoSlider({
+    effect: 'fade',
+    controlNav: false,
+  })
+}
